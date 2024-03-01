2a3350284386fe63f68507bfb72656082501b215
ne_srv <ne_srv@mail.ru>
2024-03-01 23:54:00 +0300

    autosave

--- module 1/test.py
+++ module 1/test.py
@@ -1,48 +1,12 @@
 from pprint import pprint
 
 
-
-# class int:
-#     def __init__(self, value):
-#         self.value = 5
-
-#     def __add__(self, value):
-#         return self.value * value
+class ИмяКласса:
+    pass
 
 
-# x = int(10)
-# y = 20
+a = ИмяКласса()
 
-# print(type(x))
-# print (x + y)
-# class int:
-#     def __add__(self, other):
-#         return (self * other)
+print(type(ИмяКласса))
+print(isinstance(a, ИмяКласса))
 
-
-# def __add__(self, y):
-#     return (self * y)
-
-
-# x = 2
-# y = 5
-
-# print(x+y)
-
-# oper =
-
-# print(int.__add__.__doc__)
-
-
-class int:
-    def __init__(self, value):
-        self.value = value
-
-    def __add__(self, value):
-        return self.value * value
-
-
-n1 = int(10)
-n2 = 5
-
-print(n1 + n2)
