78e929d7d721cacb4973c3ae0a05ce67fe1351ee
ne_srv <ne_srv@mail.ru>
2024-03-19 02:27:04 +0300

    autosave

---
+++ module 2/code/sol_03_points.py
@@ -0,0 +1,57 @@
+from math import hypot
+from dataclasses import dataclass
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def distance(a:Point, b:Point):
+    """ Расстояние между двумя точками """
+    return ((a.x - b.x) **  2 +(a.y - b.y) ** 2) ** 0.5
+
+# Дан список точек
+points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
+
+# Задание: найдите точку наиболее удаленную от начала координат и выведите ее координаты
+## Вариант 1
+max_distance = 0
+nul = Point(0, 0)
+for i in range(len(points)):
+    if max_distance < distance(nul, points[i]):
+        max_distance = distance(nul, points[i])
+        point_index = i
+print(f"Координаты наиболее удаленной точки = "
+      f"Point({points[point_index].x}, {points[point_index].y})")
+## Округление значения типа float до двух десятичных разрядов
+print(f'Расстояние {round(max_distance, 2)}')
+
+## Вариант 2
+max_len_line = 0
+for index in range(len(points)):
+    if (value := math.hypot(points[index].x, points[index].y)) > max_len_line:
+        max_len_line = value
+        point_index = index
+
+print(f"Координаты наиболее удаленной точки = "
+      f"Point({points[point_index].x}, {points[point_index].y})")
+print(f'Расстояние {round(max_len_line, 2)}')
+
+
+# Вариант 3
+@dataclass
+class Point:
+    x: int
+    y: int
+
+    def __post_init__(self):
+        self.length = hypot(self.x, self.y)
+
+
+points = [Point(2, 7), Point(12, 7), Point(
+    5, -2), Point(10, -16), Point(-12, 0)]
+
+all_len = (point.length for point in points)
+
+print(max(all_len))
---
+++ module 2/code/test.py
@@ -0,0 +1,24 @@
+from math import hypot
+
+res = hypot(3,4)
+print (res)
+
+# сортировка
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def distance(a: Point, b: Point) -> float:
+    """ Расстояние между двумя точками """
+    return ((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5
+
+
+# Ломаная линия задана произвольным количеством последовательных точек
+points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
+
+# Задание: Найдите длину ломаной линии
+# Вариант 1
