724b307039c2f98c2630cbaa276334c30118ad5b
ne_srv <ne_srv@mail.ru>
2024-03-20 13:37:30 +0300

    autosave

---
+++ module 3/code/_06_net.py
@@ -0,0 +1,24 @@
+
+
+class IpAddress:
+
+    def __init__(self, ip=''):
+        self.ip = list(map(int, ip.split('.')))
+
+    def show_bin_ip(self):
+        ip_address = (f'{ip:b}'.rjust(8, '0') for ip in self.ip)
+        print(*ip_address)
+
+    def __mul__(self, other):
+        res = (ip1 & ip2 for ip1, ip2 in zip(self.ip, other.ip))
+        return tuple(res)
+
+    def __repr__(self):
+        return str(*self.ip)
+
+
+ip_address = IpAddress('142.9.227.146')
+ip_address.show_bin_ip()  # 10001110 00001001 11100011 10010010
+subnet_mask = IpAddress('255.255.192.0')
+ip_net = ip_address * subnet_mask
+print(ip_net) #(142, 9, 192, 0)
