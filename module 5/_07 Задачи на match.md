09c6319769e9ba0afc0c689c9f5cf40d13a6d475
ne_srv <ne_srv@mail.ru>
2024-03-25 13:51:30 +0300

    autosave

---
+++ module 5/_07 Задачи на match.md
@@ -0,0 +1,34 @@
+## Задача 1
+
+Имеется следующий фрагмент программы с функцией parse_json и словарем json_data:
+```python
+def parse_json(data):
+    match data:
+        case {'id': ids, 'data': [_, {'login': login}, _, _]}:
+            return ids, login
+    
+    return None
+
+json_data = {'id': 2, 'access': False, 'data': ['26.05.2023', {'login': '1234', 'email': 'xxx@mail.com'}, 2000, 56.4]}
+
+```
+
+С помощью оператора match/case в функцию parse_json добавьте в самое начало шаблон для выделения значения ключа access с проверкой на тип bool и для выделения даты (первое значение списка) из поля data с проверкой, что data является списком. 
+
+Возвратите выделенные два значения в виде кортежа в формате (access, date).
+
+
+## Задача 2
+
+Имеется следующий фрагмент программы с функцией parse_json и словарем json_data:
+
+```python
+def parse_json(data):
+    match data:
+        case {'id': ids, 'data': [_, {'login': login}, _, _]}:
+            return ids, login
+    
+    return None
+```
+json_data = {'id': 2, 'access': True, 'data': ['26.05.2023', {'login': '1234', 'email': 'xxx@mail.com'}, 2000, 56.4]}
+С помощью оператора match/case в функцию parse_json добавьте в самое начало шаблон для выделения значений ключей login и email с проверкой, что они являются строками и при условии, что поле access принимает значение True. Возвратите выделенные два значения в виде кортежа в формате (login, email).
--- module 5/code/iter.py
+++ module 5/code/iter.py
@@ -39,6 +39,18 @@ class IterColumn:
         return iter(self.lst)
 
 
+lst = [[11, 12, 13],
+       [21, 22, 23],
+       [31, 32, 33]
+       ]
+
+iter_matrix = IterColumn(lst, 2)
+
+
+for x in iter_matrix:  # последовательный перебор всех элементов столбца списка: x12, x22, ..., xM2
+    print(x)
+
+
 # ===================================
     
 
@@ -49,3 +61,52 @@ class IterColumn:
 
     def __iter__(self):
         return iter(x[self.column] for x in self.lst)
+
+
+
+# ========================================
+    
+
+class Cell:
+
+    def __init__(self, data=0):
+        self.__data = data
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        self.__data = value
+
+
+class TableValues:
+
+    def __init__(self, rows, cols, type=int):
+        self.__type = type
+        self.__rows = rows
+        self.__cols = cols
+        self.__cells = tuple(tuple(Cell() for _ in range(cols))
+                             for _ in range(rows))
+
+    def __check_index(self, index):
+        r, c = index
+        if not (0 <= r < self.__rows) or not (0 <= c < self.__cols):
+            raise IndexError('неверный индекс')
+
+    def __getitem__(self, item):
+        self.__check_index(item)
+        r, c = item
+        return self.__cells[r][c].data
+
+    def __setitem__(self, key, value):
+        self.__check_index(key)
+        if type(value) != self.__type:
+            raise TypeError('неверный тип присваивания данных')
+        r, c = key
+        self.__cells[r][c].data = value
+
+    def __iter__(self):
+        for row in self.__cells:
+            yield (x.data for x in row)
