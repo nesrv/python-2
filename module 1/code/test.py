fda8f11e2c490bce642ddd3044b7e823d773558d
nesrv <ne_srv@mail.ru>
2024-05-28 10:17:53 +0300

    autosave

--- module 1/code/test.py
+++ module 1/code/test.py
@@ -1,10 +1,28 @@
-x = 3.5
-print(x.as_integer_ratio())
+# x = 3.5
+# print(x.as_integer_ratio())
 
-y = 15
+# y = 15
 
-print(y.bit_count())
-print(type(y))
+# print(y.bit_count())
+# print(type(y))
 
-x = bin(y)
-print(x)
+# x = bin(y)
+# print(x)
+
+from dataclasses import dataclass, field
+
+@dataclass
+class Point:
+    x: int
+    y: int
+    length: float = field(init=False)
+ 
+
+    def __post_init__(self):   
+        self.length = (self.x ** 2 + self.y ** 2) ** 0.5
+    
+    
+p1 = Point(1,2)
+print(p1)
+print(p1.__dict__)
+print(p1.length)
