0162df92da763a64f177dc4ff7fc3fc70d793756
nesrv <ne_srv@mail.ru>
2024-05-30 10:52:27 +0300

    autosave

--- module 4a/code/_00.py
+++ module 4a/code/_00.py
@@ -1,30 +1,33 @@
 # Реализовать класс, наследуемый от класса list и переопределить метод append,
 # реализовав в нем не добавление элементов, как по умолчанию, а удаление последнего элемента.
-
 # Разрешить сложение списка со строкой и наоборот
-class Vector(list):
+class list(list):
+    
     def __str__(self):
         return ",".join(map(str, self))
-
+    
     def append(self):
         self.pop()
+    def append_first(self, value):
+        self.insert(0, value)
 
     def __add__(self, other):
-        self += other
+        self += [other]
         return self
 
     def __radd__(self, other):
-        v = Vector(other)
+        v = list(other)
         return v + self
 
 
-v = Vector([1, 2, 3])
-# print(v)
 
-v.append()
-# print(v)
 
-v1 = v + 'python'
-print(v1)  # 1,2,p,y,t,h,o,n
-v2 = 'python' + v  # p,y,t,h,o,n,1,2
-print(v2)
+a = list((1,2,3,4))
+a.append()
+print(a)
+a.append_first(100)
+a = a + "python"
+a = 'c++' + a
+print(a)
+
+
